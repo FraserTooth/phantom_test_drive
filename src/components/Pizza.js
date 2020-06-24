112e57f4a07028d73fb0ca4fb8c20f34e59b0a78
Fraser Tooth <frasertooth@gmail.com>
2020-06-24 22:37:45 +0900

    Example built

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -2,6 +2,6 @@ export default function Pizza(slices) {
   return `
     <div id="pizza-box">
       <h1 data-phantom="${slices}" id="slices-h1">${slices}</h1>
-    </div>
+	</div>
   `;
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import reduxStore from "./store.js";
 import Pizza from "./components/Pizza.js";
 
 function phantomComponent() {
+  const { slices } = data();
   return `
-    ${Pizza()} // inject the Pizza component from above
+    ${Pizza(slices)}
   `;
 }
 
 export const { fire, data, launch } = phantom(reduxStore, phantomComponent);
 
 launch(); // initial render
+
+document.addEventListener("click", eatPizza);
+
+function eatPizza(e) {
+  if (e.target.id === "slices-h1") {
+    fire({ type: "EAT_SLICE" }); // fire an action to the store
+  }
+}
